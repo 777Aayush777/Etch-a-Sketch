95f8fe55f5cdfcc7d5f5841941534a1de7849348
Aayush Patil <75661245+777Aayush777@users.noreply.github.com>
2024-12-30 11:49:12 +0530

    add user input

--- esui.js
+++ esui.js
@@ -4,81 +4,142 @@ const thirtysix=document.querySelector(".thirtysix");
 const sixtyfour=document.querySelector(".sixtyfour");
 const eight = document.querySelector(".eight");
 const onetwoeight=document.querySelector(".onetwoeight");
+const usrinpt = document.querySelector(".usrinpt");
 
-
-eight.addEventListener("click",()=>{
-    for(let i=0;i<=63;i++){
+usrinpt.addEventListener("click", () => {
+    const inpt = prompt("Enter your Grid");
+    const gridSize = Number(inpt);
+    
+    if (gridSize > 100) {
+        alert("Enter a number less than 100");
+        return;
+    }
+    
+    container.innerHTML = '';
+    
+    const pad = 35 / (gridSize / 8);
+    
+    for (let i = 0; i < (gridSize * gridSize); i++) {
         const box = document.createElement("div");
-        box.classList="gridbox"
-        box.style.padding= "20px"
+        box.classList = "gridbox";
+        box.style.padding = `${pad}px`;
         container.appendChild(box);
-
-        box.addEventListener("mouseover",()=>{
-            box.style.backgroundColor="black";
-            box.classList="blackbox";
-            box.style.padding="20px";
+        
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "black";
+            box.classList = "blackbox";
+            box.style.padding = `${pad}px`;
         })
     }
 })
 
 
-sixteen.addEventListener("click",()=>{
-    for(let i=0;i<=255;i++){
+
+
+
+for (let i = 0; i < 8 * 8; i++) {
+    const box = document.createElement("div");
+    box.classList = "gridbox"
+    box.style.padding = "35px"
+    container.appendChild(box);
+
+    box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "black";
+        box.classList = "blackbox";
+        box.style.padding = "35px";
+    })
+}
+
+
+eight.addEventListener("click",()=>{
+    
+    container.innerHTML = '';
+
+    for(let i=0;i<=63;i++){
         const box = document.createElement("div");
         box.classList="gridbox"
-        box.style.padding= "10px"
+        box.style.padding= "35px"
         container.appendChild(box);
 
         box.addEventListener("mouseover",()=>{
             box.style.backgroundColor="black";
             box.classList="blackbox";
-            box.style.padding="10px";
+            box.style.padding="35px";
+        })
+    }
+    
+})
+
+
+sixteen.addEventListener("click",()=>{
+    
+    container.innerHTML = '';
+    
+    for(let i=0;i<=255;i++){
+        const box = document.createElement("div");
+        box.classList="gridbox"
+        box.style.padding= "17.5px"
+        container.appendChild(box);
+
+        box.addEventListener("mouseover",()=>{
+            box.style.backgroundColor="black";
+            box.classList="blackbox";
+            box.style.padding="17.5px";
         })
     }
 })
 
 thirtysix.addEventListener("click",()=>{
+
+    container.innerHTML = '';
+
     for(let i=0;i<=1023;i++){
         const box = document.createElement("div");
         box.classList="gridbox"
-        box.style.padding= "5px"
+        box.style.padding= "8.75px"
         container.appendChild(box);
 
         box.addEventListener("mouseover",()=>{
             box.style.backgroundColor="black";
             box.classList="blackbox";
-            box.style.padding="5px";
+            box.style.padding="8.75px";
         })
     }
 
 })
 
 sixtyfour.addEventListener("click",()=>{
+    
+    container.innerHTML = '';
+
     for(let i=0;i<=4095;i++){
         const box = document.createElement("div");
         box.classList="gridbox"
-        box.style.padding= "2.5px"
+        box.style.padding= "4.375px"
         container.appendChild(box);
 
         box.addEventListener("mouseover",()=>{
             box.style.backgroundColor="black";
             box.classList="blackbox";
-            box.style.padding="2.5px";
+            box.style.padding="4.375px";
         })
     }
 })
 
 onetwoeight.addEventListener("click",()=>{
+
+    container.innerHTML = '';
+
     for(let i=0;i<=16383;i++){
         const box = document.createElement("div");
         box.classList="gridbox"
-        box.style.padding= "1.25px"
+        box.style.padding= "2.1875px"
         container.appendChild(box);
 
         box.addEventListener("mouseover",()=>{
             box.style.backgroundColor="black";
             box.classList="blackbox";
-            box.style.padding="1.25px";
+            box.style.padding="2.1875px";
         })
     }
 })
